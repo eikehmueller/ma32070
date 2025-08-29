f0a6ddf87ba10493af8e53399a2fc9ad5868569a
Eike Mueller <eike.h.mueller@gmail.com>
2025-08-29 09:54:11 +0100

    Allow running without tests

--- src/parallel/matmul/driver.py
+++ src/parallel/matmul/driver.py
@@ -54,6 +54,11 @@ parser.add_argument(
     help="No parallel communication (leads to incorrect results)?",
 )
 
+parser.add_argument(
+    "--nocheck",
+    action="store_true",
+    help="Do not check correctness of results?",
+)
 
 args, _ = parser.parse_known_args()
 
@@ -78,33 +83,44 @@ if rank == 0:
     print(f" r = {r:8d}")
     print(f" overlap = {args.overlap}")
     print(f" nocomm  = {args.nocomm}")
-
+    print(f" nocheck = {args.nocheck}")
+    print()
 rng = np.random.default_rng(seed=411587)
-# Global matrices
-A = rng.normal(size=(n, m))
-B = rng.normal(size=(m, r))
-# Result for testing
-C_true = A @ B
+if not args.nocheck:
+    # Global matrices
+    A = rng.normal(size=(n, m))
+    B = rng.normal(size=(m, r))
+    # Result for testing
+    C_true = A @ B
 t_elapsed = 0
 niter = 0
-while t_elapsed < 10.0:
-    A_loc = np.array(A[rank * n_loc : (rank + 1) * n_loc, :])
-    B_loc = np.array(B[rank * m_loc : (rank + 1) * m_loc, :])
-    C_loc = np.zeros(shape=(n_loc, r))
+t_target = 10.0
+while t_elapsed < t_target:
+    if args.nocheck:
+        A_loc = rng.normal(size=(n_loc, m))
+        B_loc = rng.normal(size=(m_loc, r))
+        C_loc = np.zeros(shape=(n_loc, r))
+    else:
+        A_loc = np.array(A[rank * n_loc : (rank + 1) * n_loc, :])
+        B_loc = np.array(B[rank * m_loc : (rank + 1) * m_loc, :])
+        C_loc = np.zeros(shape=(n_loc, r))
     comm.Barrier()
     t_start = MPI.Wtime()
     parallel_matmul(A_loc, B_loc, C_loc, args.overlap, args.nocomm)
     comm.Barrier()
     t_finish = MPI.Wtime()
-    C = np.zeros(shape=(n, r))
-    comm.Allgather(C_loc, C)
     delta_t = comm.allreduce(t_finish - t_start, MPI.MAX)
     t_elapsed += delta_t
     niter += 1
 
 t_elapsed /= niter
 
-if rank == 0:
+if not args.nocheck:
+    C = np.zeros(shape=(n, r))
+    comm.Allgather(C_loc, C)
     nrm = np.linalg.norm(C - C_true) / np.linalg.norm(C_true)
-    print(f"error norm = {nrm:8.4e}")
+
+if rank == 0:
+    if not args.nocheck:
+        print(f"error norm = {nrm:8.4e}")
     print(f"elapsed time = {t_elapsed:8.2e} s")

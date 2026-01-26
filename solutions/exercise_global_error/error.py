0ec46085fe88e2f2d0311cb6b2f31d5cdf43c29a
Eike Mueller <eike.h.mueller@gmail.com>
2026-01-26 14:26:50 +0000

    Copied file from finiteelement repo

--- solutions/exercise_global_error/error.py
+++ solutions/exercise_global_error/error.py
@@ -1,4 +1,4 @@
-"""Computation of finite element error norm"""
+"""Computation of finite element L2 error norm"""
 
 import numpy as np
 
@@ -6,7 +6,7 @@ __all__ = ["error_norm"]
 
 
 def error_norm(u_numerical, u_exact, quad):
-    """Compute L2 error norm
+    """Compute L2 error norm ||u-u_{exact}||_{L_2(Omega)}
 
     :arg u_numerical: numerical solution, must be an instance of Function
     :arg u_exact: function, must be callable everywhere in the domain
@@ -15,9 +15,9 @@ def error_norm(u_numerical, u_exact, quad):
     fs = u_numerical.functionspace
     mesh = fs.mesh
     fs_coord = mesh.coordinates.functionspace
-    # Finite element of function space
+    # finite element of function space
     element = fs.finiteelement
-    # Finite element of coordinate field
+    # finite element of coordinate field
     element_coord = fs_coord.finiteelement
     error_nrm_2 = 0
     for alpha in range(mesh.ncells):
@@ -27,23 +27,23 @@ def error_norm(u_numerical, u_exact, quad):
         # local and global dof-indices of numerical solution
         ell = range(element.ndof)
         ell_g = fs.local2global(alpha, ell)
-        # Local dof-vector of coordinate field
+        # local dof-vector of coordinate field
         x_dof_vector = mesh.coordinates.data[ell_g_coord]
-        # Local dof-vector of numerical solution
+        # local dof-vector of numerical solution
         u_numerical_dof_vector = u_numerical.data[ell_g]
-        # Quadrature points and weights
+        # quadrature points and weights
         zeta_q = np.asarray(quad.nodes)
         w_q = quad.weights
-        # Tabulation of finite element basis functions at quadrature points
+        # tabulation of finite element basis functions at quadrature points
         T = element.tabulate(zeta_q)
-        # Tabulation of coordinate basis functions and their gradients at quadrature points
+        # tabulation of coordinate basis functions and their gradients at quadrature points
         T_coord = element_coord.tabulate(zeta_q)
         T_coord_grad = element_coord.tabulate_gradient(zeta_q)
-        # Physical coordinates at quadrature point
+        # physical coordinates at quadrature point
         x_q = np.einsum("qla,l->qa", T_coord, x_dof_vector)
-        # Evaluation of exact solution at quadrature points
+        # evaluation of exact solution at quadrature points
         u_exact_q = u_exact(x_q.T)
-        # Error at quadrature points
+        # error at quadrature points
         error_q = u_exact_q - T @ u_numerical_dof_vector
         # Jacobian and its determinant
         J = np.einsum("l,qlab->qab", x_dof_vector, T_coord_grad)
